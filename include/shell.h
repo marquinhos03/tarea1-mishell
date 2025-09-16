a58921cf94cf6a4eeb0fa64a48a41f45537a4fba
marquinhos03 <mliguempibozzano@gmail.com>
2025-09-15 21:04:44 -0300

    feat: read_line() función para leer linea de entrada

---
+++ include/shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/*Librerias*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+/*Prototipos de función*/
+char *read_line();
+
+#endif
