8319b3a3564ca5d5b33e3e6c4877a48c45a1af49
marquinhos03 <mliguempibozzano@gmail.com>
2025-09-17 18:42:22 -0300

    refactor: Extraer funciones para manejo de entrada a io_utils.c

--- include/shell.h
+++ include/shell.h
@@ -7,8 +7,16 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <stdbool.h>
+
+#define TOKEN_DELIM " \t\r\n\a\""
 
 /*Prototipos de función*/
 char *read_line();
+char **parse_line(char *line);
+int new_process(char **args);
+int execute_args(char **args);
+
+int own_exit(char **);
 
 #endif
---
+++ src/io_utils.c
@@ -0,0 +1,64 @@
+#include "include/shell.h"
+
+char *read_line() {
+    char *line = NULL;
+    size_t bufsize = 0;
+
+    /** 
+     * getline() para leer la linea completa 
+     * Si getline() retorno -1 se maneja el error
+    */
+    if (getline(&line, &bufsize, stdin) == -1) {
+        if (feof(stdin)) {
+            /* Caso EOF (Ctrl+D), salida con éxito de la shell. */ 
+            printf("\n");
+            free(line);
+            exit(EXIT_SUCCESS);
+        }
+        else {
+            free(line);
+            perror("error al leer la linea");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return line;
+}
+
+/**
+ * Dividir el string de entrada en multiples strings (substrings)
+ */
+char **parse_line(char *line) {
+    int bufsize = 64;
+    int c = 0;
+    char **tokens = malloc(bufsize * sizeof(char *));   /* sizeof(char *) -> 8 bytes */
+    char *token;
+
+    if (!tokens) {
+        perror("error al asignar memoria en parse_line: tokens\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Busca en line hasta el primer delimitador */
+    token = strtok(line, TOKEN_DELIM);
+    while (token != NULL) {
+        tokens[c] = token;
+        c++;
+
+        if (c >= bufsize) {
+            bufsize += bufsize;
+            tokens = realloc(tokens, bufsize * sizeof(char *));
+
+            if (!tokens) {
+                perror("error al reasignar memoria en parse_line: tokens");
+                exit(EXIT_FAILURE);
+            }
+        }
+        /* NULL para que continue desde la ultima posición */
+        token = strtok(NULL, TOKEN_DELIM);
+    }
+    /* NULL para indicar el final de los argumentos de la linea */
+    tokens[c] = NULL;
+    
+    return tokens;
+}
